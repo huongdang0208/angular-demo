91f1dbb6ea4ca2c4328c4346db465fc97b18fe25
Thu_huong <dangthithuhuong02082002@gmail.com>
2021-12-04 11:39:56 +0700

    pushgit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,21 +8,38 @@ import {Product} from '../product.model';
 })
 
 export class ProductsComponent implements OnInit {
-   @Input() products: any;
-  @Output() onRemove = new EventEmitter();
+    @Input() products: any;
+    @Output() onRemove = new EventEmitter();
+    @Output() onUpdateQuantity = new EventEmitter();
+
   constructor (){}
     ngOnInit(){
 
     }
-    removeProduct(productId: number): void{
+
+    removeProduct(productId: number){
       this.onRemove.emit(productId)
 
     }
+
     // updateQuantity(element){
     //   console.log(element.value)
     // }
     // onRemove(){
     //   alert('remove?')
     // }
+    inputQuantity(id: number, inputElement: HTMLInputElement) {
+      const value = inputElement.value;
+      const intValue = parseInt(value);
+
+      if (intValue < 1) {
+        inputElement.value = -intValue + '';
+      } else if (value.length > 2) {
+        inputElement.value = value.slice(0, 2);
+      }
+
+      this.onUpdateQuantity.emit({ id, quantity: parseInt(inputElement.value) || '' });
+  }
+
 
 }
